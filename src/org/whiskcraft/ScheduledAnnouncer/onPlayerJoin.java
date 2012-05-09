f7ab00805dd33284c72f943d33e0864bbe67ff58
Gavin Casdorph <whisk@w-c.us>
2012-05-08 21:24:26 -0800

    Added the code to get the MOTD from the config.

--- src/org/whiskcraft/ScheduledAnnouncer/onPlayerJoin.java
+++ src/org/whiskcraft/ScheduledAnnouncer/onPlayerJoin.java
@@ -1,5 +1,18 @@
 package org.whiskcraft.ScheduledAnnouncer;
 
-public class onPlayerJoin {
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class onPlayerJoin implements Listener {
+	
+	String string = "Hello World!";
+	this.getConfig().set("announcement.MOTD", stringValue);
+	
+	private final AnnouncerPlugin plugin;
+	
+	public void onPlayerLogin(PlayerLoginEvent event) {
+		event.getPlayer().sendMessage(string);
+		
+	}
 
 }
