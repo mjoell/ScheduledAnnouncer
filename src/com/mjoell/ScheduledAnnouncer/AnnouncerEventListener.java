9ffb02a77f28bc6085bc6bea905b2a862ce87efe
Michael Joell <me@mjoell.com>
2014-07-19 05:58:54 -0700

    Change paths

---
+++ src/com/mjoell/ScheduledAnnouncer/AnnouncerEventListener.java
@@ -0,0 +1,38 @@
+package com.mjoell.ScheduledAnnouncer;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class AnnouncerEventListener implements Listener {
+	
+	private final AnnouncerPlugin plugin;
+	
+	public AnnouncerEventListener(AnnouncerPlugin plugin)
+	{
+		this.plugin = plugin;
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(final PlayerJoinEvent event) {
+		
+		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
+		{
+			public void run()
+			{
+				if(plugin.motdEnabled == true)
+				{
+					if(event.getPlayer().hasPermission(AnnouncerPermissions.RECEIVER))
+					{
+						plugin.sendMotd(event.getPlayer());
+						return;
+					}
+				}
+				else
+				{
+					return;
+				}
+			}
+		}, 3); 
+	}
+}
